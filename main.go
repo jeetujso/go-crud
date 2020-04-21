aac7c8924a26b68fd60d7040efd5510834fa0f2e
jeetu <jeetu.jha25@gmail.com>
2020-04-21 13:14:47 +0530

    crud with memory data

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/jeetujso/router"
 )
 
 func main() {
-	r := router.Router()
-	// fs := http.FileServer(http.Dir("build"))
-	// http.Handle("/", fs)
 	fmt.Println("Starting server on the port 8000...")
-
-	log.Fatal(http.ListenAndServe(":8000", r))
 }
